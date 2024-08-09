5f9eac78872c16ee8437d6ae92dd97da4948f4f5
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-08-09 17:07:15 +0900

    fix: change audio file to mono mixed) ;

--- python_practice/ch1_import_wav_file.py
+++ python_practice/ch1_import_wav_file.py
@@ -1,5 +1,6 @@
 from scipy.io.wavfile import read
 import matplotlib.pyplot as plt
+import numpy as np
 # read function's input: wav file
 # read function's output: sampling rate and audio data array
 # I wrote and composed this sound file: 'jaehyun_sound_for_study.wav'
@@ -9,5 +10,11 @@ import matplotlib.pyplot as plt
 print(f" variable \"fs\" is sampling rate, fs is {fs}")
 print(f"then, var x is matrix that processed matrix by this library")
 print(f"x.size is {x.size}, x/fs={x.size/fs}, it means length of the wav file")
-print(f"If the X/FS result is twice the actual output length, it means that the file is a stereo file and has two channels, one for the left and one for the right.")
+print(f"(If the X/FS result is twice the actual output length, it means that the file is a stereo file and has two channels, one for the left and one for the right.)")
+
+plt.plot(x)
+plt.show()
+
+t= np.arange(x.size)/float(fs) #it generates time axis!
+print(t)
 
