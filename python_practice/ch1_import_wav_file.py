62c318664cd7032765771ebd9483269090e12dbc
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-08-09 16:44:22 +0900

    add: add comment that note free to use my file

--- python_practice/ch1_import_wav_file.py
+++ python_practice/ch1_import_wav_file.py
@@ -1,8 +1,10 @@
 from scipy.io.wavfile import read
-#read function's input: wav file
-#read function's output: sampling rate
-#i wrote this sound file
 
-(fs, x)=read('jaehyun_sound_for_study.wav')
+# read function's input: wav file
+# read function's output: sampling rate and audio data array
+# I wrote and composed this sound file: 'jaehyun_sound_for_study.wav'
+# You are free to use this sound file for any purpose, including study, research, and creative projects.
+
+(fs, x) = read('jaehyun_sound_for_study.wav')
 print(fs)
 print(x)
