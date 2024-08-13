93c6755f668c6ab944f58af2fb6d23bf1cf8f564
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-08-13 21:00:06 +0900

    add DFT for sinewave

---
+++ python_practice/ch2_DFT_sine_input.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+N=64
+k0=7
+X=np.array([])
+x=np.cos(2*np.pi*k0/N*np.arange(N))
+nv = np.arange(-N/2,N/2) # time indexes
+kv = np.arange(-N/2,N/2) # index for k
+
+for k in kv:
+  s=np.exp(1j*2*np.pi*k/N*nv)
+  X=np.append(X,sum(x*np.conjugate(s)))
+
+plt.plot(kv,abs(X))
+plt.axis([-N/2,N/2-1,0,N])
+
+plt.show()
