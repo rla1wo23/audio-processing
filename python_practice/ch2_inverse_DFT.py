345bdfdf6bd56739ea1d17a723ea0cd28d15b1cc
rla1wo23@gmail.com <rla1wo23@gmail.com>
2024-08-13 21:13:36 +0900

    add: Inverse DFT

--- python_practice/ch2_inverse_DFT.py
+++ python_practice/ch2_inverse_DFT.py
@@ -2,17 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N=64
-k0=7
-X=np.array([])
+k0=7.5
 x=np.cos(2*np.pi*k0/N*np.arange(N)) #generate cos wave
+
+X=np.array([])
 nv = np.arange(-N/2,N/2) # time indexes
-kv = np.arange(-N/2,N/2) # index for k
+kv = np.arange(-N/2,N/2) # index for k  
 
 for k in kv:
   s=np.exp(1j*2*np.pi*k/N*nv)
   X=np.append(X,sum(x*np.conjugate(s)))
+y=np.array([])
+for n in nv:
+  s=np.exp(1j*2*np.pi*n/N*kv)
+  y=np.append(y,1.0/N*sum(X*s))
 
-plt.plot(kv,abs(X))
-plt.axis([-N/2,N/2-1,0,N])
-
+plt.plot(kv,y)
+plt.axis([-N/2,N/2-1,-1,1])
 plt.show()
